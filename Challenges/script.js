1e9bbaea697e726ac78caa6e5aff3eb5ef9bee1c
Antje Myriknas <antje.myriknas@yahoo.co.uk>
2022-05-18 10:52:05 +0100

    Codewar Challenges - Solution

--- Challenges/script.js
+++ Challenges/script.js
@@ -533,3 +533,18 @@ const reverseSeq = (n) => {
   }
   return arr;
 };
+
+// Opposites attract
+function lovefunc(flower1, flower2) {
+  if (
+    (flower1 % 2 === 0 && flower2 % 2 === 0) ||
+    (flower1 % 2 !== 0 && flower2 % 2 !== 0)
+  ) {
+    return false;
+  } else if (
+    (flower1 % 2 !== 0 && flower2 % 2 === 0) ||
+    (flower1 % 2 === 0 && flower2 % 2 !== 0)
+  ) {
+    return true;
+  }
+}
