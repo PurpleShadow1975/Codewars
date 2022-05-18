efc244b6c8c8f5f0b43a51c6f11208d13b016429
Antje Myriknas <antje.myriknas@yahoo.co.uk>
2022-05-18 10:59:07 +0100

    Codewar Challenges - Solution

--- Challenges/script.js
+++ Challenges/script.js
@@ -548,3 +548,12 @@ function lovefunc(flower1, flower2) {
     return true;
   }
 }
+
+// Are You Playing Banjo?
+function areYouPlayingBanjo(name) {
+  if (name[0] === "R" || name[0] === "r") {
+    return name + " plays banjo";
+  } else {
+    return name + " does not play banjo";
+  }
+}
