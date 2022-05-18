09bb8ed5cee006df463d1456d805fc4f1281abbc
Antje Myriknas <antje.myriknas@yahoo.co.uk>
2022-05-18 10:18:43 +0100

    Codewar Challenges - Solution

--- Challenges/script.js
+++ Challenges/script.js
@@ -510,3 +510,17 @@ function fakeBin(x) {
 function hero(bullets, dragons) {
   return bullets / dragons >= 2 ? true : false;
 }
+
+// Calculate BMI
+function bmi(weight, height) {
+  let bmi = weight / (height * height);
+  if (bmi <= 18.5) {
+    return "Underweight";
+  } else if (bmi <= 25) {
+    return "Normal";
+  } else if (bmi <= 30) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
+}
